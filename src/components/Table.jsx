8d76b47cacd9dbfb6ea40b0edd5366b3a2f1dcd1
Stephen Bradshaw <root@Elliss-MacBook-Pro.local>
2024-08-25 15:25:13 -0500

    Graded assignment 6 complete

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,17 +1,31 @@
+import './styling.css'
+
 const Table = ({ sat }) => {
+
   return (
+ 
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
-     </tbody>
-   </table>
+     {sat.map((data) => (
+        
+        <tr key={data.id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          {data.operational ? <td>Actove</td> : <td>Inactive</td>}   
+        </tr>
+      ))}
+
+    </tbody>
+  </table>
   );
 };
 
